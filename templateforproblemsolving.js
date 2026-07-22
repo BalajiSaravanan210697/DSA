18f1926b5a4a52417000215e0f1b1ee6e1342f38
Balaji Saravanan <BalajiSaravanan@fico.com>
2026-07-22 20:34:30 +0530

    solution for longest substring problem

---
+++ templateforproblemsolving.js
@@ -0,0 +1,43 @@
+/*Problem Name
+- Problem:
+- Difficulty:
+- Date:
+
+1.Did I understand the problem? Yes/No
+
+2.Do I know the constraints? Yes/No
+Constraints:
+-
+-
+
+3.Frame the Test Data - Positive,Edge,Negative - DONE/NOT DONE
+
+4.Do I already know how to solve it? Yes/No
+
+Possible approaches:
+1.
+2.
+3.
+
+5.Write Pseudocode & dry run it with test data - DONE/NOT DONE
+Pseudocode:
+
+6.Write the Code in Notepad++ & dry run it with test data - DONE/NOT DONE
+
+7.Run the Code in Editor - Executed - Success/Failure
+
+8.Debug the code and resolve the failures - DONE/NOT DONE/NOT REQUIRED
+
+9.try with Optimized solution - DONE/NOT DONE
+
+10.Review the Learning from this Problem 
+
+What pattern did I learn?
+
+What mistake did I make?
+
+What should I remember for next time?
+
+*/
+
+
